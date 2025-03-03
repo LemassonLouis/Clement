5f362cfefe05e004d5abb33c4cc17e2b0ad23de7
DragoLeForgeron <louis.lemasson.pro@gmail.com>
2025-03-03 23:40:37 +0100

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,13 +2,14 @@ import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
+import { createTables } from "@/database/db";
 
-// Empêche l'écran de démarrage de se cacher immédiatement
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   useEffect(() => {
     SplashScreen.hideAsync();
+    createTables();
   }, []);
 
   return (
