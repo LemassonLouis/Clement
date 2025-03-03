a83a57bf362d2466ce5250d6194c0f367e479801
DragoLeForgeron <louis.lemasson.pro@gmail.com>
2025-03-03 23:39:55 +0100

    Create session.tsx

---
+++ database/session.tsx
@@ -0,0 +1,39 @@
+import { getDB } from "./db";
+
+export const createSession = async (dateTimeStart: string, dateTimeEnd: string|null = null, sexWithoutProtection: boolean = false) => {
+  const db = await getDB();
+
+  const statement = await db.prepareAsync(
+    'INSERT INTO Session (date_time_start, date_time_end, sexWithoutProtection) VALUES (?, ?, ?)'
+  );
+
+  try {
+    const result = await statement.executeAsync([dateTimeStart, dateTimeEnd, sexWithoutProtection ? 1 : 0]);
+    console.log('Session insérée avec succès'); // TEMP ?
+    return result.lastInsertRowId;
+  } catch (error) {
+    console.error('Erreur lors de la création de la session:', error);
+  } finally {
+    await statement.finalizeAsync();
+  }
+};
+
+export const getSessionByDate = async (dateTimeStart: string, dateTimeEnd: string) => {
+  const db = await getDB();
+
+  try {
+    const sessions = await db.getAllAsync("SELECT * FROM Session WHERE date_time_start >= ? AND date_time_end <= ?", [dateTimeStart, dateTimeEnd] );
+    console.log('Sessions récupérées avec succès');
+    return sessions.map(session => deserializeSession(session));
+  } catch (error) {
+    console.error('Erreur lors de la récupération des sessions:', error);
+  }
+}
+
+const deserializeSession = (session: any) => {
+  session.date_time_start = new Date(session.date_time_start);
+  session.date_time_end = new Date(session.date_time_end);
+  session.sexWithoutProtection = session.sexWithoutProtection ? true : false;
+  return session;
+}
+
