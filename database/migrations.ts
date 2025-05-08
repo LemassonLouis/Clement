b5900261bc5e46f777dcd316bbbc4da0f6d9f3af
Lemasson Louis <144131578+LemassonLouis@users.noreply.github.com>
2025-05-08 22:44:02 +0200

    fix: Stabilize migrations

--- database/migrations.ts
+++ database/migrations.ts
@@ -13,16 +13,16 @@ export async function migrateTables(): Promise<void> {
   const USER_ACTIVE_MIGRATION = 2;
 
   // TEMP : reset pragma version
-  await resetMigration("User", [
-    'wantFiveMinutesRemainingNotification',
-    'wantOneHourRemainingNotification',
-    'wantTwoHoursRemainingNotification',
-    'wantObjectiveMinExtraReachedNotification',
-    'wantObjectiveMinReachedNotification',
-    'wantObjectiveMaxReachedNotification',
-    'wantObjectiveMaxExtraReachedNotification',
-    'isActive',
-  ]);
+  // await resetMigration("User", [
+  //   'wantFiveMinutesRemainingNotification',
+  //   'wantOneHourRemainingNotification',
+  //   'wantTwoHoursRemainingNotification',
+  //   'wantObjectiveMinExtraReachedNotification',
+  //   'wantObjectiveMinReachedNotification',
+  //   'wantObjectiveMaxReachedNotification',
+  //   'wantObjectiveMaxExtraReachedNotification',
+  //   'isActive',
+  // ]);
 
   const [{ user_version }] = await db.getAllAsync<{ user_version: number }>(
     'PRAGMA user_version;'
