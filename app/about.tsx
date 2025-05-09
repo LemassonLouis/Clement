f9984a9bb0c714828d537b49ca6e32f2f25969e8
DragoLeForgeron <louis.lemasson.pro@gmail.com>
2025-05-09 18:05:31 +0200

    chore: About setting

--- app/about.tsx
+++ app/about.tsx
@@ -1,6 +1,8 @@
 import { useNavigation } from "expo-router";
 import { useEffect } from "react";
-import { Text } from "react-native";
+import { Linking, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import Constants from 'expo-constants';
+
 
 export default function About() {
   const navigation = useNavigation();
@@ -9,7 +11,48 @@ export default function About() {
     navigation.setOptions({ title: "À propos"});
   }, [navigation]);
 
+  const appVersion = Constants.expoConfig?.version;
+
+  const handleSourceCodePress = () => {
+    Linking.openURL('https://github.com/LemassonLouis/Clement');
+  }
+
   return (
-    <Text>TODO</Text>
+    <View style={styles.container}>
+      <View style={styles.textContainer}>
+        <Text style={styles.textTitle}>Version de l'application</Text>
+        <Text>{appVersion}</Text>
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.textTitle}>Application développé par</Text>
+        <Text>LEMASSON Louis</Text>
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.textTitle}>Application designé par</Text>
+        <Text>LEMASSON Louis</Text>
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.textTitle}>Code source de l'application</Text>
+        <TouchableOpacity onPress={handleSourceCodePress}>
+          <Text>https://github.com/LemassonLouis/Clement</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
   )
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    width: '80%',
+    marginHorizontal: 'auto',
+    alignItems: 'center',
+  },
+  textContainer: {
+    alignItems: 'center',
+    marginVertical: 10,
+  },
+  textTitle: {
+    fontWeight: 500,
+  }
+});
